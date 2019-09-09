f24f1131d82985fb2d501d0546f94596db1ff82e
PouyaT <pouya.g.tavakoli-1@ou.edu>
2019-09-09 12:24:11 -0500

    refined turn detection algo and added a new file for output images

--- Testing_Sandbox.py
+++ Testing_Sandbox.py
@@ -8,25 +8,24 @@ import math
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
 
-img_file = os.path.join(os.getcwd(), "Images")
+img_file = os.path.join(os.getcwd(), "Input_Images")
 img_file_list = os.listdir(img_file)
 
-video_file = os.path.join(os.getcwd(), "Videos")
+video_file = os.path.join(os.getcwd(), "Input_Videos")
 video_file_list = os.listdir(video_file)
 
 # use this as a queue. enqueue: list.append() deque: list.pop(0)
 glob_avg_x_loc = list()
+glob_avg_y_loc = list()
 
 
 # returns a filtered image and unfiltered image. This is needed for white lines on green grass
-def image_seg_opencv():
+# outputs are numpy arrays
+def image_seg_opencv(og_image):
     kernel = np.ones((3, 3), np.uint8)
 
-    img = cv2.imread(os.path.join(img_file, img_file_list[0]))
-    # used as refernce images
-    og_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    result = og_image.copy()
+    img = cv2.cvtColor(og_image, cv2.COLOR_BGR2HSV)
     # create a lower bound for a pixel value
     lower = np.array([0, 0, 200])
     # create an upper bound for a pixel values
@@ -56,21 +55,14 @@ def image_seg_opencv():
     return result, og_image
 
 
-# takes in the filered image and the og image to place red lines on top of
-def pipeline(image):
+# takes in the filtered image and the og image to place red lines on top of
+def pipeline(pre_or_post_filtered_image, original_overlay_image):
     # read in desired image
     # image = mpimg.imread(os.path.join(img_file, img_file_list[2]))
 
     # gives the height and width of the image from the dimensions given
     height = image.shape[0]
     width = image.shape[1]
-
-    # print(width, height)
-
-    # printing out some stats and plotting the image
-    # print('This image is:', type(image), 'with dimensions:', image.shape)
-    # plt.imshow(image)
-    # plt.show().
 
     # curvy road test width-50
     # region_of_interest_vertices = [
@@ -100,7 +92,7 @@ def pipeline(image):
     # plt.show()
 
     # convert to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    gray_image = cv2.cvtColor(pre_or_post_filtered_image, cv2.COLOR_RGB2GRAY)
 
     # call Canny Edge Detection
     cannyed_image = cv2.Canny(gray_image, 100, 200)
@@ -117,7 +109,7 @@ def pipeline(image):
     lines = cv2.HoughLinesP(cropped_image, rho=6, theta=np.pi / 60, threshold=75,
                             lines=np.array([]), minLineLength=40, maxLineGap=25)
 
-    line_image = draw_lines(image, lines)
+    line_image = draw_lines(original_overlay_image, lines)
 
     # this code is needed when we know we have two lanes on the left or right side of the images.
 
@@ -165,18 +157,21 @@ def pipeline(image):
     #     ]],
     # )
 
-    # gets the centered x location of the current frame
-    frame_x_loc = current_x_loc(lines)
+    # gets the centered x and y location of the current frame
+    frame_x_loc, frame_y_loc = current_x_n_y_loc(lines)
+
+    print(lines)
 
     plt.figure()
     plt.imshow(line_image)
     plt.show()
 
-    desired_loc(frame_x_loc)
+    desired_loc(frame_x_loc, frame_y_loc)
 
     return line_image
 
 
+# takes in an image and a list of points (vertices) to crop the image
 def region_of_interest(img, vertices):
     # define a blank matrix that matches the iamge height/width.
     mask = np.zeros_like(img)
@@ -193,6 +188,8 @@ def region_of_interest(img, vertices):
     return masked_image
 
 
+# teaks in an image, a list of lines from HoughedLinesP and draws red lines on top of the passed in image
+# outputs the images
 def draw_lines(img, lines, color = [255,0,0], thickness=3):
     # If there are no lines to draw, exit
     if lines is None:
@@ -216,10 +213,11 @@ def draw_lines(img, lines, color = [255,0,0], thickness=3):
     return img
 
 
+# testing out cropping mechanism
 def crop_images():
 
     # read in desired image
-    image = mpimg.imread(os.path.join(img_file, img_file_list[0]))
+    image = mpimg.imread(os.path.join(img_file, img_file_list[4]))
 
     # images I'm using are 540x960x3
     height = image.shape[0]
@@ -253,8 +251,9 @@ def crop_images():
 
     # takes in a list of coordinates that are lines
 
-# takes in a list of lines and figures out the current x loc of the frame
-def current_x_loc(lines):
+
+# takes in a list of lines and figures out the current x loc and y loc of the frame
+def current_x_n_y_loc(lines):
     left_line_x = []
     left_line_y = []
     right_line_x = []
@@ -262,6 +261,8 @@ def current_x_loc(lines):
 
     for line in lines:
         for x1, y1, x2, y2 in line:
+            # using the slope might not work for curved lanes. Might just need to split the image in half at
+            # an x pixel point and everything to the left is left side and everything to the right is right side
             slope = (y2 - y1) / (x2 - x1)  # <-- Calculating the slope.
             if slope <= 0:  # <-- If the slope is negative, left group.
                 left_line_x.extend([x1, x2])
@@ -270,7 +271,7 @@ def current_x_loc(lines):
                 right_line_x.extend([x1, x2])
                 right_line_y.extend([y1, y2])
 
-    # find average x and y coordinates of each side of the line
+    # finds the median x and y coordinates of each side of the line
     try:
         right_line_x_med = statistics.median(right_line_x)
         right_line_y_med = statistics.median(right_line_y)
@@ -293,42 +294,58 @@ def current_x_loc(lines):
         min_y_avg = (right_line_min_y + left_line_min_y) / 2
     except statistics.StatisticsError:
         # returns a -1 which is normally impossible to return
-        return -1
+        return -1, -1
 
-    return min_x_avg
+    return min_x_avg, min_y_avg
 
 
-def desired_loc(curr_x_loc):
+# takes in the current x and y location and sees if they differ from the avg location from the previous 15
+# frames to see if the a turn is required or not
+# TODO test out this method idk how accurate this is if at all
+def desired_loc(curr_x_loc, curr_y_loc):
+    # gets current angle (in degrees)
+    curr_angle = np.arctan2(curr_x_loc, curr_y_loc) * (180 / np.pi)
+
     # catches the -1 and returns nothing and ends the function
-    if curr_x_loc == -1:
+    if curr_x_loc == -1 or curr_y_loc == -1:
         return None
     # makes sure there ar at least 15 values in the list before doing turn evaluation
-    if len(glob_avg_x_loc) < 15:
+    if len(glob_avg_x_loc) < 15 and len(glob_avg_y_loc) < 15:
         glob_avg_x_loc.append(curr_x_loc)
+        glob_avg_y_loc.append(curr_y_loc)
         print('list to short')
     # print out which direction the robot should be going and update the lit (que)
     else:
         avg_x = statistics.mean(glob_avg_x_loc)
-        if curr_x_loc > avg_x:
-            print('turn right dif is: ', curr_x_loc - avg_x)
+        avg_y = statistics.mean(glob_avg_y_loc)
+
+        # gets the average angle of the frames
+        avg_angle = np.arctan2(avg_x, avg_y) * (180 / np.pi)
+
+        if curr_angle > avg_angle:
+            print('turn right dif is: ', curr_angle - avg_angle)
             glob_avg_x_loc.pop(0)
             glob_avg_x_loc.append(curr_x_loc)
-        elif curr_x_loc < avg_x:
-            print('turn left dif is: ', curr_x_loc - avg_x)
+            glob_avg_y_loc.pop(0)
+            glob_avg_y_loc.append(curr_y_loc)
+        elif curr_angle < avg_angle:
+            print('turn left dif is: ', curr_angle - avg_angle)
             glob_avg_x_loc.pop(0)
             glob_avg_x_loc.append(curr_x_loc)
+            glob_avg_y_loc.pop(0)
+            glob_avg_y_loc.append(curr_y_loc)
 
 
 if __name__ == '__main__':
-    # image = mpimg.imread(os.path.join(img_file, img_file_list[5]))
-    # pipeline(image)
-    # images = image_seg_opencv()
-    # white_line_on_road_driver(image, image)
+    image = mpimg.imread(os.path.join(img_file, img_file_list[4]))
+    img_list = image_seg_opencv(image)
+    pipeline(img_list[0], img_list[1])
     # crop_images()
-    video = os.path.join(video_file, video_file_list[3])
-    print(video)
-    white_output = 'curved_road_partial_output.mp4'
-    clip1 = VideoFileClip(video)
-    white_clip = clip1.fl_image(pipeline)
-    white_clip.write_videofile(white_output, audio=False)
+    # images = image_seg_opencv()
+    # video = os.path.join(video_file, video_file_list[3])
+    # print(video)
+    # white_output = 'curved_road_partial_output.mp4'
+    # clip1 = VideoFileClip(video)
+    # white_clip = clip1.fl_image(pipeline)
+    # white_clip.write_videofile(white_output, audio=False)
 
